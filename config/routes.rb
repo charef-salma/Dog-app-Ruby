f6304b35795ee536c6a7b33107a4656c0b50dc65
charef-salma <charefsalma1998@gmail.com>
2024-01-14 21:18:51 +0100

    first commit

---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+  # get 'dogs/index'
+  # get 'dogs/show'
+  # get 'dogs/new'
+  # get 'dogs/edit'
+  resources :dogs
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
